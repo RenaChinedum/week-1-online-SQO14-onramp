ef8aba37d1ef427a489c825e7f5f9f502e467659
RenaChinedum <donrenachinedu@gmail.com>
2022-11-01 21:39:13 -0700

    Java challenges
    java challenges update

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,12 @@
 function countTruthy(arr) {
 
+        truthycount = 0
+            for ( const element of arr) {
+                if (element) {
+                    truthycount++
+                }
+            }
+            return truecount 
 }
+
 module.exports = countTruthy
--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,5 +1,12 @@
 function filterList(arr) {
- 
+    var filteredlist = [];
+    for (var i = 0; i < arr.length; i++) {
+      if (typeof arr[i] === "number") {
+        filteredlist.push(arr[i]);
+      }
+    }
+    return filteredlist;
+   
  }
 
   module.exports =filterList
